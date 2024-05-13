6edb3d517e4a538abe988f2cd071f40ba0bcbb0c
DJ Kehoe <donald.j.kehoe@gmail.com>
2024-05-12 21:43:05 -0400

    added the GFC_String class

---
+++ include/gfc_string.h
@@ -0,0 +1,107 @@
+#ifndef __GFC_STRING_H__
+#define __GFC_STRING_H__
+
+typedef struct
+{
+    char   *buffer; /**<contains the character data*/
+    size_t  length; /**<how long the character array is*/
+    size_t  size;   /**<how much data has been allocated*/
+}GFC_String;
+
+
+/**
+ * @brief allocate an empty string
+ * @return NULL if memory is full, a pointer to a GFC_String otherwise
+ */
+GFC_String *gfc_string_new();
+
+/**
+ * @brief allocate an empty string, pre-allocatign a set buffer length
+ * @param size how many characters you want to pre-allocate.  if zero, this is a no-op
+ * @return NULL if memory is full, a pointer to an empty GFC_String otherwise of size
+ */
+GFC_String *gfc_string_new_size(Uint32 size);
+
+
+/**
+ * @brief free a previously new'd string
+ * @param string the string to free
+ */
+void gfc_string_free(GFC_String *string);
+
+/**
+ * @brief create a new string presetting it to the size needed and setting the buffer to the provided character
+ * @param text the text to save into this string, if NULL, identical to gfc_string_new();
+ * @return NULL on error, your created string otherwise
+ */
+GFC_String *gfc_string(const char *text);
+
+/**
+ * @brief create a new string presetting it to the size needed and setting the format buffer provided.  Similar to printf
+ * @param text the text to save into this string, if NULL, identical to gfc_string_new();
+ * @param ... the variables to populate the text format
+ * @return NULL on error, your created string otherwise
+ */
+GFC_String *gfc_stringf(const char * text,...);
+
+/**
+ * @brief get a pointer to the character buffer in the string as a standard C str.
+ * @param string the string to get from
+ * @return NULL if not set, or the character array otherwise
+ */
+const char *gfc_string_text(GFC_String *string);
+
+/**
+ * @brief append the text to the end of the string, expanding as necessary
+ * @param string the string to add to
+ * @param text the text to add to it
+ */
+void gfc_string_append(GFC_String *string,const char *text);
+
+/**
+ * @brief append to the string a formated string
+ * @param string the string to append to
+ * @param format the format text to append
+ * @param ... the variables used to populate the formatted text
+ */
+void gfc_string_appendf(GFC_String *string,const char *format,...);
+
+/**
+ * @brief add the text from one string to the end of another
+ * @param string the string to add to
+ * @param add the string to add from
+ */
+void gfc_string_concat(GFC_String *string,const GFC_String *add);
+
+/**
+ * @brief append the text to the start of the string, expanding as necessary
+ * @param string the string to add to
+ * @param text the text to add to it
+ */
+void gfc_string_prepend(GFC_String *string,const char *text);
+
+/**
+ * @brief append the text to the start of the string, expanding as necessary
+ * @param string the string to add to
+ * @param text the text to add to it formatted like a printf format string
+ * @param ... the variables used to populate the formatted text
+ */
+void gfc_string_prependf(GFC_String *string,const char *text,...);
+
+/**
+ * @brief perform a string compare against the two strings
+ * @param string1 one string to check
+ * @param string2 second string to check
+ * @return -3 if there are any NULL values, otherwise the results a strcmp on the two buffers
+ */
+int gfc_string_cmp(GFC_String *string1,GFC_String *string2);
+
+/**
+ * @brief perform a string compare against the two strings
+ * @param string1 one string to check
+ * @param string2 second string to check
+ * @return -3 if there are any NULL values, -2 if the strings are different length, otherwise the results a strcmp on the two buffers
+ */
+int gfc_string_l_cmp(GFC_String *string1,GFC_String *string2);
+
+#endif
---
+++ src/gfc_string.c
@@ -0,0 +1,235 @@
+#include <stdarg.h>
+
+#include "simple_logger.h"
+
+#include "gfc_types.h"
+#include "gfc_text.h"
+#include "gfc_string.h"
+
+#define GFC_STRING_DEFAULT_LENGTH 256
+
+GFC_String *gfc_string_new()
+{
+    return gfc_string_new_size(GFC_STRING_DEFAULT_LENGTH);
+}
+
+GFC_String *gfc_string_new_size(Uint32 size)
+{
+    GFC_String *string;
+    if (!size)return NULL;
+    string = gfc_allocate_array(sizeof(GFC_String),1);
+    if (!string)
+    {
+        slog("failed to allocate memory for a new GFC_String");
+        return NULL;
+    }
+    string->buffer = gfc_allocate_array(sizeof(char),size);
+    if (!string->buffer)
+    {
+        slog("failed to allocate memory for new GFC_String buffer");
+        gfc_string_free(string);
+        return NULL;
+    }
+    string->size = size;
+    return string;
+}
+
+
+void gfc_string_free(GFC_String *string)
+{
+    if (!string)return;
+    if (string->buffer)
+    {
+        free(string->buffer);
+    }
+    free(string);
+}
+
+GFC_String *gfc_string(const char *text)
+{
+    GFC_String *string;
+    Uint32 length,size;
+    if (!text)return gfc_string_new();
+    length = strlen(text);
+    if (!length)return gfc_string_new();
+    for (size = GFC_STRING_DEFAULT_LENGTH;length > size;size*=2);//find the needed buffer size for the string length
+    string = gfc_string_new_size(size);
+    if (!string)return NULL;
+    strcpy(string->buffer,text);
+    string->length = length;
+    return string;
+}
+
+char *gfc_string_get_formated_text(size_t *sizeOut,const char *format,va_list ap)
+{
+    int r;
+    size_t size;
+    char *buffer;
+    buffer = gfc_allocate_array(sizeof(char),GFC_STRING_DEFAULT_LENGTH);
+    if (!buffer)return NULL;
+    size = GFC_STRING_DEFAULT_LENGTH;
+    while ((r = vsprintf(buffer,format,ap)) < 0)
+    {
+        size *= 2;
+        if (buffer)free(buffer);
+        buffer = gfc_allocate_array(sizeof(char),size);
+        if (!buffer)
+        {
+            return NULL;
+        }
+    }
+    if (sizeOut)*sizeOut = size;
+    return buffer;
+}
+
+GFC_String *gfc_stringf(const char * text,...)
+{
+    va_list ap;
+    size_t size = GFC_STRING_DEFAULT_LENGTH;
+    char *buffer;
+    GFC_String *string;
+    if (!text)return gfc_string_new();
+    buffer = gfc_allocate_array(sizeof(char),size);
+    if (!buffer)return NULL;
+    va_start(ap,text);
+    while (vsprintf(buffer,text,ap) < 0)
+    {
+        size *= 2;
+        if (buffer)free(buffer);
+        buffer = gfc_allocate_array(sizeof(char),size);
+        if (!buffer)
+        {
+            return NULL;
+        }
+        va_end(ap);
+        va_start(ap,text);
+    }
+    va_end(ap);
+    string = gfc_allocate_array(sizeof(GFC_String),1);
+    if (!string)
+    {
+        slog("failed to allocated memory for GFC_String");
+        free(buffer);
+        return NULL;
+    }
+    string->buffer = buffer;
+    string->size = size;
+    string->length = strlen(buffer);
+    return string;
+}
+
+const char *gfc_string_text(GFC_String *string)
+{
+    if (!string)return NULL;
+    return string->buffer;
+}
+
+void gfc_string_append(GFC_String *string,const char *text)
+{
+    char *buffer;
+    size_t newlength,size;
+    if ((!string)||(!text))return;
+    newlength = strlen(text) + string->length;
+    for (size = string->size;newlength > size;size*=2);//make sure we can fit
+    if (size > string->size)
+    {
+        //we need to expand;
+        buffer = gfc_allocate_array(sizeof(char),size);
+        sprintf(buffer,"%s%s",string->buffer,text);
+        free(string->buffer);
+        string->buffer = buffer;
+        string->length = newlength;
+        string->size = size;
+        return;
+    }
+    buffer = &string->buffer[string->length];
+    strcpy(buffer,text);
+    string->length = newlength;
+}
+
+void gfc_string_appendf(GFC_String *string,const char *format,...)
+{
+    va_list ap;
+    size_t size = GFC_STRING_DEFAULT_LENGTH;
+    char *buffer;
+    if (!format)return;
+    buffer = gfc_allocate_array(sizeof(char),size);
+    if (!buffer)return;
+    va_start(ap,format);
+    while (vsprintf(buffer,format,ap) < 0)
+    {
+        size *= 2;
+        if (buffer)free(buffer);
+        buffer = gfc_allocate_array(sizeof(char),size);
+        if (!buffer)
+        {
+            return;
+        }
+        va_end(ap);
+        va_start(ap,format);
+    }
+    va_end(ap);
+    gfc_string_append(string,buffer);
+    free(buffer);
+}
+
+void gfc_string_concat(GFC_String *string,const GFC_String *add)
+{
+    if ((!string)||(!add)||(!string->buffer)||(!add->buffer))return;
+    gfc_string_append(string,add->buffer);
+}
+
+void gfc_string_prepend(GFC_String *string,const char *text)
+{
+    char *buffer;
+    size_t newlength,size;
+    if ((!string)||(!text))return;
+    newlength = strlen(text) + string->length;
+    for (size = string->size;newlength > size;size*=2);//make sure we can fit
+    buffer = gfc_allocate_array(sizeof(char),size);
+    sprintf(buffer,"%s%s",text,string->buffer);
+    free(string->buffer);
+    string->buffer = buffer;
+    string->length = newlength;
+    string->size = size;
+}
+
+void gfc_string_prependf(GFC_String *string,const char *format,...)
+{
+    va_list ap;
+    size_t size = GFC_STRING_DEFAULT_LENGTH;
+    char *buffer;
+    if (!format)return;
+    buffer = gfc_allocate_array(sizeof(char),size);
+    if (!buffer)return;
+    va_start(ap,format);
+    while (vsprintf(buffer,format,ap) < 0)
+    {
+        size *= 2;
+        if (buffer)free(buffer);
+        buffer = gfc_allocate_array(sizeof(char),size);
+        if (!buffer)
+        {
+            return;
+        }
+        va_end(ap);
+        va_start(ap,format);
+    }
+    va_end(ap);
+    gfc_string_prepend(string,buffer);
+    free(buffer);
+}
+
+int gfc_string_cmp(GFC_String *string1,GFC_String *string2)
+{
+    if ((!string1)||(!string2)||(!string1->buffer)||(!string2->buffer))return -3;
+    return strcmp(string1->buffer,string2->buffer);
+}
+
+int gfc_string_l_cmp(GFC_String *string1,GFC_String *string2)
+{
+    if ((!string1)||(!string2)||(!string1->buffer)||(!string2->buffer))return -3;
+    return gfc_strlcmp(string1->buffer,string2->buffer);
+}
+
+/*eol@eof*/
