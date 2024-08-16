b24a5423e4b1541e1fb1800edaf866e52ca189c7
DJ Kehoe <donald.j.kehoe@gmail.com>
2024-08-16 12:45:13 -0400

    updating audio defaults.  Didn't help

--- src/gfc_audio.c
+++ src/gfc_audio.c
@@ -30,7 +30,7 @@ void gfc_audio_init(
 {
     int flags = 0;
 
-    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024)==-1)
+    if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048)==-1)
     {
         slog("Failed to open audio: %s\n", SDL_GetError());
         return;
